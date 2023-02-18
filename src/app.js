d9bc1d603e8f5432049f449e63ca42728f5ea915
jesuse2r <jesuse2rr@gmail.com>
2023-02-18 01:38:51 +0000

    https://github.com/jesuse2r/javascipt1.git

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["dog", "cat", "turtle", "bird"];
+  let what = ["ate", "pissed", "crushed", "broke"];
+  let toWhat = ["my homework", "my car", "my shoe", "my bike"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let toWhatRandom = Math.floor(Math.random() * toWhat.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[whoRandom]} ${what[whatRandom]} ${toWhat[toWhatRandom]} ${when[whenRandom]}`;
+
+  let newElement = document.createElement("p");
+  newElement.innerHTML = excuse;
+
+  let excuseContainer = document.querySelector("#excuse");
+
+  excuseContainer.appendChild(newElement);
 };
+
+console.log;
